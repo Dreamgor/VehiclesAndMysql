da59fdc91532296c91ff90e208d09a5db5dbad22
dreamgor <jacob_water@hotmail.com>
2019-02-28 20:33:37 +0100

    This could work. addvehicle() changed

--- src/se/lexicon/garage/MysqlConnection.java
+++ src/se/lexicon/garage/MysqlConnection.java
@@ -2,6 +2,7 @@ package se.lexicon.garage;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MysqlConnection {
 
@@ -44,8 +45,46 @@ public class MysqlConnection {
     return vehicles;
   }
 
-  public void addVehicle(){
-    //Den vehicle som skickas med som argument skall sparas i databasen
+  public void addVehicle(Garage g){
+    Scanner sc = new Scanner(System.in);
+    Connection conn = null;
+    Statement stat = null;
+    ResultSet rs = null;
+
+    String brand = sc.nextLine();
+    int topspeed = sc.nextInt();
+
+    Car c = new car(brand, topspeed);
+
+    g.park(c);
+
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", "root", "Deadthief?55");
+      System.out.println("Connected to Sakila database!");
+
+      stat = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+
+
+      while(rs.next()){
+        if(rs.getString("type").equals("empty")){
+          rs = stat.executeQuery("Insert Into vehicle(maker, topspeed) Values(" + brand + ", "
+                  + topspeed + ")");
+        }
+      }
+    }catch(SQLException e){
+      e.printStackTrace();
+    }finally{
+      if(rs != null){
+        rs.close();
+      }
+      if(stat != null){
+        stat.close();
+      }
+      if(conn != null){
+        conn.close();
+      }
+    }
   }
 
   public static void main(String[] args) {
